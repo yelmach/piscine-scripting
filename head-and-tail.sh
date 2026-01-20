ca3fea17ccd8606488b4afdb2b03b2ce5e9c6e81
yelmach <yassineelmach0@gmail.com>
2026-01-20 11:41:01 +0100

    head tail and command grouping

--- head-and-tail.sh
+++ head-and-tail.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt | {head -1&&tail -1}
+curl -s https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt | { head -n 1; tail -n 1; }
