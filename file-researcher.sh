68c54c1918ef7d5375b8b7abff7ffb5d7c2d62ff
yelmach <yassineelmach0@gmail.com>
2026-01-20 10:23:31 +0100

    grep with -n flag

--- file-researcher.sh
+++ file-researcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grep "It takes 12 honey" file-researcher/facts.txt
-grep "year" file-researcher/facts.txt
+grep -n "It takes 12 honey" file-researcher/facts.txt
+grep -n "year" file-researcher/facts.txt
