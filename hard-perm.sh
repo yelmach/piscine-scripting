a96c9e4b18e7de188e7c33bf09cb0ed682444a7d
yelmach <yassineelmach0@gmail.com>
2026-01-20 13:22:23 +0100

    chmod command

---
+++ hard-perm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+chmod 401 hard-perm/0
+chmod 402 hard-perm/1
+chmod 604 hard-perm/2
+chmod 777 hard-perm/3
+chmod 510 hard-perm/4
+chmod 460 hard-perm/5
+chmod 460 hard-perm/6
+chmod 510 hard-perm/7
+chmod 604 hard-perm/8
+chmod 402 hard-perm/9
+chmod 401 hard-perm/A
