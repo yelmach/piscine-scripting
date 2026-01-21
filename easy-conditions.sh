ee33f5ecb03aa1c9f32950bd721cb77c43b7d308
yelmach <yassineelmach0@gmail.com>
2026-01-21 11:15:31 +0100

    conditions

---
+++ easy-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+(( X > Y )) && echo "true" || echo "false"
+
+# [ "$X" -gt "$Y" ] && echo "true" || echo "false"
+
+# if [ "$X" -gt "$Y" ]; then
+#     echo "true"
+# else
+#     echo "false"
+# fi
