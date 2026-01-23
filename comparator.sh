771438ed83c9847897316b493eb865605b97a87f
yelmach <yassineelmach0@gmail.com>
2026-01-23 09:48:54 +0100

    comparator

---
+++ comparator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! $# -eq 2 ]; then
+    echo "Error: The script only works with two arguments!"
+elif [[ ! $1 =~ ^[0-9]*$ ]] || [[ ! $2 =~ ^[0-9]*$ ]]; then
+    echo "Error: Only two numeric arguments are acceptable!"
+elif [ $1 -gt $2 ]; then
+    echo "$1 > $2"
+elif [ $1 -lt $2 ]; then
+    echo "$1 < $2"
+else
+    echo "$1 = $2"
+fi
