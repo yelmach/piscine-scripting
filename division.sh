2593241b4c5178ecad92630196fd3531aa712aed
yelmach <yassineelmach0@gmail.com>
2026-01-23 08:55:28 +0100

    division

--- division.sh
+++ division.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [[ $# -ne 2 ]]; then
-    echo "Error: two numbers must be provided."
+    echo "Error: two numbers must be provided"
 
 elif [[ ! $1 =~ ^-?[0-9]+$ ]] || [[ ! $2 =~ ^-?[0-9]+$ ]]; then
-    echo "Error: both arguments must be integers."
+    echo "Error: both arguments must be integers"
 
 elif [[ $2 -eq 0 ]]; then
-    echo "Error: division by zero is not allowed."
+    echo "Error: division by zero is not allowed"
 
 else
 echo "$1 / $2" | bc
