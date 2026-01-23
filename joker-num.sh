7220817158aec09f94bed5dec009b8142ac04613
yelmach <yassineelmach0@gmail.com>
2026-01-23 12:03:09 +0100

    guessing number

---
+++ joker-num.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -ne 1 ] || [[ ! $1 =~ ^[0-9]*$ ]] || [ $1 -gt 100 ] || [ $1 -lt 1 ]; then
+    echo "Error: wrong argument"
+    exit 1
+fi
+
+number=$1
+
+for ((i = 5; i > 0;))
+do
+    echo "Enter your guess ($i tries left):"
+    read guess
+    
+    if [[ ! $guess =~ ^[0-9]*$ ]] || [ $guess -gt 100 ] || [ $guess -lt 1 ]; then
+        continue
+    fi
+
+    if [ $guess -lt $number ]; then
+        echo "Go up"
+    fi
+
+    if [ $guess -gt $number ]; then
+        echo "Go down"
+    fi
+
+    if [ $guess -eq $number ]; then
+        echo "Congratulations, you found the number in $(( 5 - $i + 1 )) moves!"
+        exit
+    fi
+
+    ((i--))
+done
+
+echo "You lost, the number was $number"
