c571ef0edac81db9e4e8aada09c2369698f60a8e
yelmach <yassineelmach0@gmail.com>
2026-01-23 09:38:52 +0100

    read user input

---
+++ greatest-of-all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+max=0
+
+for i in {1..10}
+do
+    read -p "Enter a number: " num
+
+    if [[ ! $num =~ ^[0-9]+$ ]]; then
+        echo "ERROR: Invalid input only positive numerical characters are allowed"
+        exit 1
+    fi
+
+    if (( num > 10000 )); then
+        echo "ERROR: The number entered is too large"
+        exit 1
+    fi
+
+    if (( num > max )); then
+        max=$num
+    fi
+done
+
+echo "The largest number is $max"
