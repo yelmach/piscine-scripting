d21ef4cf1f528f00046224ff0d1de8aa3937bf1f
yelmach <yassineelmach0@gmail.com>
2026-01-26 12:05:37 +0100

    clean the list

--- clean_the_list.py
+++ clean_the_list.py
@@ -2,9 +2,4 @@ def clean_list(list):
     if not list:
         return list
     
-    formatted = [item.strip().capitalize() for item in list]  
-      
-    if "Milk" not in list:
-        list.append("Milk")
-    
-    return [f"{i}/ {item}" for i, item in enumerate(formatted, start=1)]  
+    return [f"{i}/ {item.strip().capitalize()}" for i, item in enumerate(list, start=1)]  
