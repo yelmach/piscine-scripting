b0862b407a230cb1215a53b01fba6a96a99cd66a
yelmach <yassineelmach0@gmail.com>
2026-01-26 11:08:20 +0100

    operations

---
+++ numerical_operations.py
@@ -0,0 +1,16 @@
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def power(a, b):
+    return a ** b
+
+def square_root(a):
+    return math.sqrt(a, 11)
