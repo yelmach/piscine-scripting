a7fc1b282f996edfc0dca55cad31d1d887707d9f
yelmach <yassineelmach0@gmail.com>
2026-01-26 12:09:07 +0100

    clean the list

--- clean_the_list.py
+++ clean_the_list.py
@@ -2,4 +2,7 @@ def clean_list(list):
     if not list:
         return list
     
+    if 'milk' not in list:
+        list.append('milk')
+    
     return [f"{i}/ {item.strip().capitalize()}" for i, item in enumerate(list, start=1)]  
