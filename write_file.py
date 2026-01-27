1a8f0fde41869649b503d822008302c78ddc1157
yelmach <yassineelmach0@gmail.com>
2026-01-27 14:04:33 +0100

    write in a file

--- write_file.py
+++ write_file.py
@@ -1,6 +1,5 @@
 def to_do(tasks):
-    for task in tasks:
-        formatted_date = task[0].strftime("%A %d %B %Y")
-    
-        with open("output.txt", "a") as file:
+    with open("output.txt", "w") as file:
+        for task in tasks:
+            formatted_date = task[0].strftime("%A %d %B %Y")
             file.write(f"{formatted_date}: {task[1]}\n")
