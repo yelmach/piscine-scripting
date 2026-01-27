c243aa79b022f32928bf49c48d6ac1421ddc8e65
yelmach <yassineelmach0@gmail.com>
2026-01-27 13:41:51 +0100

    regex

--- string_processing.py
+++ string_processing.py
@@ -2,6 +2,6 @@ import re
 
 def tokenize(sentence):
     lower_sentence = sentence.lower()
-    tokens = re.findall(r'\w+', lower_sentence)
+    tokens = re.findall(r'[a-z0-9]+', lower_sentence)
     
     return tokens
