238c2ded9e75d89d873545dffadf63a109d2227c
yelmach <yassineelmach0@gmail.com>
2026-01-27 15:27:23 +0100

    recursive search

---
+++ credentials_searches.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+def find_keys(data, found_credentials):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key in ["password", "secret"]:
+                found_credentials[key] = value
+            elif isinstance(value, (dict, list)):
+                find_keys(value, found_credentials)
+                
+    elif isinstance(data, list):
+        for item in data:
+            find_keys(item, found_credentials)
+
+
+def credentials_search():
+    filename = "logs.json"
+    
+    if not os.path.exists(filename):
+        return
+
+    with open(filename, 'r') as file:
+        content = file.read()
+        data = json.loads(content)
+
+    found_credentials = {}
+    find_keys(data, found_credentials)
+
+    if found_credentials:
+        with open("credentials.json", "w") as outfile:
+            json.dump(found_credentials, outfile)
+
+if __name__ == "__main__":
+    credentials_search()
