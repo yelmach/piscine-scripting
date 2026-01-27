a4192eea5914ffd323fee8c8695dfcbea88a611d
yelmach <yassineelmach0@gmail.com>
2026-01-27 15:55:07 +0100

    flex params

---
+++ flex_function.py
@@ -0,0 +1,10 @@
+def create_person(first_name, last_name, age, gender, size=1.83, job="taxidermist"):
+    return {
+        'first_name': first_name,
+        'last_name': last_name,
+        'age': age,
+        'gender': gender,
+        'size': size,
+        'job': job,
+    }
+
